f73c10171f799afd3d78f9a0cc4cbd9c53fcae22
PBuskell <umlaut.bio.eye@gmail.com>
2016-02-23 13:47:32 -0600

    Branch MondayPM 3

---
+++ app/src/main/java/com/example/pbuskell/personalizedgreeter/TestOfNewBranch.java
@@ -0,0 +1,11 @@
+package com.example.pbuskell.personalizedgreeter;
+
+/**
+ * Created by PBuskell on 23/02/2016.
+ *
+ * This Class is to test whether or not the new branch will sync with GitHub correctly.
+ * If you are seeing this on GitHub then we now know how to create and sync branches from
+ *   Android Studio!
+ */
+public class TestOfNewBranch {
+}
